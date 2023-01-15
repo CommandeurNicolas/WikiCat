61d058b941cd7116e7bf1f82d340688a71707400
Nicolas Commandeur <comcomnico@gmail.com>
2023-01-15 01:57:35 +0100

    Initial Commit

---
+++ WikiCat/WikiCatApp.swift
@@ -0,0 +1,17 @@
+//
+//  WikiCatApp.swift
+//  WikiCat
+//
+//  Created by Nicolas Commandeur on 15/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct WikiCatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
